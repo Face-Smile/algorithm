c1cd0844d4dd8ca57349ee68f75c472b777394a1
Face-Smile <5245323+Face-Smile@user.noreply.gitee.com>
2021-02-19 14:07:11 +0800

    二分查找: 查找k个离x最近的元素

--- binarySearch/findClosestElements/Solution.java
+++ binarySearch/findClosestElements/Solution.java
@@ -1,5 +1,6 @@
 package algorithm.binarySearch.findClosestElements;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.IntStream;
@@ -11,24 +12,53 @@ import java.util.stream.IntStream;
 public class Solution {
     public static void main(String[] args) {
         Random random = new Random();
-        IntStream intStream = random.ints(1, 6);
-        int[] arr = intStream.distinct().limit(5).sorted().toArray();
+        IntStream intStream = random.ints(1, 30);
+        int[] arr = intStream.distinct().limit(20).sorted().toArray();
+        // int[] arr = new int[]{1, 3, 4, 4, 6, 10};
         int k = 4;
-        int x = 3;
+        int x = 30;
         IntStream.of(arr).forEach(d -> System.out.printf("%-5d", d));
         System.out.println();
         System.out.println(new Solution().findClosestElements(arr, k, x));
     }
 
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        return null;
-    }
+        // 查找元素x的位置, 不存在则找元素应该插入的位置(最后的元素除外)
+        int i = 0, j = arr.length - 1;
+        while (i < j) {
+            int mid = i + (j - i) / 2;
+            if (arr[mid] >= x) j = mid;
+            else i = mid + 1;
+        }
+        System.out.println(String.format("i:%4d j:%4d", i, j));
+        // 已此为基准,双指针确定范围, (不包括两端)
+        if (arr[i] == x) {
+            k--;
+            i--;
+            j++;
+        } else {
+            i--;
+        }
+        while (i >= 0 && j < arr.length && k > 0) {
+            if (x - arr[i] <= arr[j] - x) {
+                i--;
+            } else {
+                j++;
+            }
+            k--;
+        }
+        if (k > 0) {
+            if (i > 0) {
+                i -= k;
+            } else {
+                j += k;
+            }
+        }
+        List<Integer> res = new ArrayList<>();
+        for (i++; i < j; i++) {
+            res.add(arr[i]);
+        }
+        return res;
 
-    public List<Integer> findLeftClosestElements(int[] arr, int k, int x) {
-        return null;
-    }
-
-    public List<Integer> findRightClosestElements(int[] arr, int k, int x) {
-        return null;
     }
 }
